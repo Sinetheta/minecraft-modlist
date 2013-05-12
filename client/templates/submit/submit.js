e019c8a42fb6274f40f5a15468b4ab1f60c679b5
Kevin Attfield <k.attfield@gmail.com>
2013-05-11 22:25:04 -0700

    submit success modal

--- client/templates/submit/submit.js
+++ client/templates/submit/submit.js
@@ -1,6 +1,42 @@
+Session.set('showSuccessDialog', false);
+
+Template.submit.showSuccessDialog = function() {
+    return Session.get('showSuccessDialog');
+};
+
 Template.submit.events({
     'click #login-reveal': function() {
         setTimeout(function(){$('.dropdown-toggle').click();});
+    },
+    'click #submit-mod': function(event, template) {
+        event.preventDefault();
+        var title = template.find('[name="title"]').value;
+        var version = template.find('[name="version"]').value;
+        var author = template.find('[name="author"]').value;
+        var description = template.find('[name="description"]').value;
+        var supports = template.find('[name="supports"]').value;
+        var forge = $(template.find('[name="forge"]')).val();
+        var availability = $(template.find('[name="availability"]')).val();
+
+        event.preventDefault();
+
+        if (title.length && description.length) {
+            Meteor.call('createMod', {
+                title: title,
+                version: version,
+                author: author,
+                description: description,
+                supports: supports,
+                forge: forge,
+                availability: availability
+            }, function(error, mod) {
+                if (!error) {
+                    Session.set('showSuccessDialog', true);
+                }
+            });
+        } else {
+            //Session.set('createError', 'It needs a title and a description, or why bother?');
+        }
     }
 });
 
